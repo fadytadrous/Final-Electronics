3387f859a8e4955526c9445a02d1a21a1d279204
Eslam Khaled Korany <43995832+eslam69@users.noreply.github.com>
2020-05-24 03:06:54 +0200

    edit source code , simulation

--- source.c
+++ source.c
@@ -1,23 +1,16 @@
 #include<reg51.h>
 #include<stdio.h>
-sbit ALE=P3^3;  
-sbit OE=P3^6; 
-sbit START=P3^4; 
-sbit EOC=P3^5;  
-sbit clk=P3^7;  
-sbit ADDA=P3^0;  //Address pins for selecting input channels.
-sbit ADDB=P3^1;
-sbit ADDC=P3^2;
-#define Adc_Data P1  //ADC
+sbit ALE=P0^4;  
+sbit OE=P0^7; 
+sbit START=P0^5; 
+sbit EOC=P0^6;  
+
+sbit ADDA=P0^0;  //Address pins for selecting input channels.
+sbit ADDB=P0^1;
+sbit ADDC=P0^2;
+#define Adc_Data P2  //ADC
  
 int result[3],ADC_value;
-
-
-void timer0() interrupt 1 
-{
-    clk=~clk;
-}
-
 
 
 void delay(unsigned int count) 
@@ -40,7 +33,7 @@ void read_adc() //Function to drive ADC
     while(EOC==1);
     while(EOC==0);
     OE=1;
-    ADC_value=Adc_data;
+    ADC_value = Adc_Data;
     delay(1);
     OE=0;
 }
@@ -55,13 +48,11 @@ void adc()
 
 
 void main()
-{
+{		Adc_Data = 0xFF ;
     EOC=1;
     ALE=0;
     OE=0;
     START=0;
-    TMOD=0x02;
-    TH0=0xFD;
     IE=0x82;
     TR0=1;
     while(1)
