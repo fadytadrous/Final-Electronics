f880f8c64e689dbf25de2df7648ddb54eb9383f3
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2020-05-30 14:34:52 +0200

    Update source.c

--- source.c
+++ source.c
@@ -12,11 +12,11 @@ sbit ADDC=P0^2;
 #define DAC_port P3  //DAC
 
 //filtration constants
-// int last_input =-1 ; // initial state flag
-// int oldest_input = 0 ;
-int testarray[5]={0};
-unsigned char i =0;
-
+unsigned char N = 5;
+unsigned char last_input = 0;		// initial state flag
+unsigned char oldest_input = 0;
+unsigned char full = 0;
+int z[5]={0};
 
 //target is lowpass 150hz, high pass 1hz,notch filter at 50hz
 float coeff[5]= {0.2397,0.02625,0.02704,0.02785,0.02837}; 
@@ -28,93 +28,72 @@ void delay(unsigned int count)
     for(j=0;j<100;j++);
 }
 
-void Filter(int x)
-{
-	static const unsigned char N = 5 ;
-	unsigned char n;
-	int result=0; 
-	if(i<N)
-	{
-		testarray[i]= x;  
-		i++;
-	}
-	else{
-		for ( n = 0; n < (N-1); n++)
-		{
-			testarray[n] = testarray[n+1]; //shift w 7otkol value fi ely ganbha
-		}
-		testarray[N-1] = x;
-	}
-	for (n= 0 ; n< N ;  n++)  // idx =1
-    	{ 
-    	//fih hena casting ha y7sl momkn y2sr 3al output                                 
-            result += testarray[n] * coeff[ -n+N-1 ] * 255 ; //bamshy el coeff bmen el a5r lel awl
-    	}		
-    result = (result>>8); // take Higher 8 bits i.e shifting to right , 
-	DAC_port= result;
-}   
-// void filter(int x)  // x is the input from adc
-// {   
-// 	static const int N = 5 ;
-//     static int z[N] ; // static 3lshan b3d m y5ls elfunction de w yrg3lha tany yrg3 yla2y el array fe a5r values
-//     bool full = false;
-//     int n ;
-// 	result = 0 ;
-//     int y[N] ;
-
-//     if (last_input == -1 )
-//      	{
-//         for (n=0 ; n<N ;n++)
-//             z[n] = 0 ; // SET ALL elements TO ZERO
-        
-//         last_input = 0 ; //reset      
-//     	}
-
-//     z[last_input] = x ; 
-
-//     if (((last_input +1) % N )==0)
-//     	{  
-//         full = true ; 
-//         last_input = 0 ;
-//         oldest_input = (last_input+N) % N +1 ;
-
-//         }
-//     else 
-//         {
-
-//         last_input = (last_input +1) % N ;
-         
-                 
-        
-//         }
-    
-//      // circular buffer pointer ----> last einputut entry location
-//     if (!full)  
-//     {
-// 	    for (n= 0 ; n< last_input ;  n++)  // idx =1
-//     	{                                  
-//             result += z[n] * coeff[ abs( n-(N-1) ) ] * 255 ;
-//     	}          
-//     }
-      
-//     else
-//     { 
-//         for (int i =0 ; i<N ; i++)
-//         {
-//             y[i] = z[(oldest_input+i)%N];
-//         }
-//         oldest_input = (oldest_input+1)%N ;
-
-//         for (n= 0 ; n< last_input ;  n++)  // idx =1
-//     	{                                  
-//             result += y[n] * coeff[ abs( n-(N-1) ) ] * 255 ;
-// 	    }    
-
+// void Filter(int x)
+// {
+// 	static const unsigned char N = 5 ;
+// 	unsigned char n;
+// 	int result=0; 
+// 	if(i<N)
+// 	{
+// 		testarray[i]= x;  
+// 		i++;
 // 	}
-//     result = (result>>4) ; // take Higher 4 bits i.e shifting to right , 
+// 	else{
+// 		for ( n = 0; n < (N-1); n++)
+// 		{
+// 			testarray[n] = testarray[n+1]; //shift w 7otkol value fi ely ganbha
+// 		}
+// 		testarray[N-1] = x;
+// 	}
+// 	for (n= 0 ; n< N ;  n++)  // idx =1
+//     	{ 
+//     	//fih hena casting ha y7sl momkn y2sr 3al output                                 
+//             result += testarray[n] * coeff[ -n+N-1 ] * 255 ; //bamshy el coeff bmen el a5r lel awl
+//     	}		
+//     result = (result>>8); // take Higher 8 bits i.e shifting to right , 
 // 	DAC_port= result;
-// }
+// }   
 
+void filter (int x)			// x is the input from adc
+{
+  unsigned char n;
+  int result = 0;
+
+
+  z[last_input] = x;
+  last_input = (last_input + 1) % N;
+
+  if (last_input == 0)
+    {
+      full = 1;
+    }
+
+  if (full == 0)
+    {
+      for (n = 0; n < last_input; n++)	// idx =1
+		{
+	  	result += z[n] * coeff[-n+N-1] * 255;
+		}
+    }
+
+  else
+    {
+      //  oldest_input = (last_input + 1) % N;
+      for (n = 0; n < N; n++)	// idx =1
+		{
+       	result += z[(oldest_input + n) % N] * coeff[-n+N-1] * 255;  
+	  // result = z[(oldest_input + n) % N];	// for just debugging
+	  // printf ("current input is  %d  ", x);
+	  // printf ("%d  %d \t", n, result);
+	  // printf ("\n");
+		}
+      oldest_input = (last_input + 1) % N;
+
+    }
+
+  	result = (result>>8); // take Higher 8 bits i.e shifting to right , 
+	DAC_port= result;
+}
 
 void read_adc() //Function to drive ADC
 {
@@ -126,7 +105,7 @@ void read_adc() //Function to drive ADC
     while(EOC==1);
     while(EOC==0);
     OE=1;
-    Filter(Adc_Data);  // or ------> filter(Adc_Data); // I think this is a little bit faster , we will save variable assignment operation 
+    filter(Adc_Data);  // or ------> filter(Adc_Data); // I think this is a little bit faster , we will save variable assignment operation 
     delay(1);
     OE=0;
 }
